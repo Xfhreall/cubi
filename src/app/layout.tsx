739cd823d6cc6fc9db70ce0816ea9914b9b610d3
Xfhreall <farelrizqi377@gmail.com>
2026-02-06 15:16:49 +0700

    feat: enhance layout structure with NuqsAdapter and Toaster components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "@/shared/styles/globals.css";
 import { QueryProvider } from "@/shared/providers/query-provider";
 import { MainNav } from "@/shared/components/custom/main-nav";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Toaster } from "@/shared/components/shadcn/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,15 +27,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="id">
+    <html lang="id" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProvider>
-          <MainNav />
-          <main className="container mx-auto px-4 py-8">
-            {children}
-          </main>
+          <NuqsAdapter>
+            <MainNav />
+            <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8">
+              {children}
+            </main>
+            <Toaster richColors />
+          </NuqsAdapter>
         </QueryProvider>
       </body>
     </html>
